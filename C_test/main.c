29985ac110609a92662394db3b01d6ee0478c9e5
yolominwoo <minwoo9594@gmail.com>
2022-12-15 01:11:37 +0900

    Initial Commit

---
+++ C_test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  C_test
+//
+//  Created by 정민우 on 2022/12/15.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
