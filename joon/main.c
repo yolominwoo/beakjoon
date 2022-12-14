cfcf01dadb5f2ac47db72b003c7325cdb472a0da
yolominwoo <minwoo9594@gmail.com>
2022-12-15 00:57:26 +0900

    Initial Committest

--- joon/main.c
+++ joon/main.c
@@ -1,47 +1,6 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
-    printf("Hello, World!test\n");
-    printf("Hello, git\n");
     printf("Hello, World!test\n");
     printf("Hello, git\n");
     return 0;
