f88e85d88de19b46c94e5d0c8a98c1edb478964d
yolominwoo <minwoo9594@gmail.com>
2022-12-15 00:57:26 +0900

    Initial Committest

--- joon/main.c
+++ joon/main.c
@@ -5,7 +5,8 @@ int main(int argc, const char * argv[]) {
     printf("Hello, git\n");
     
     int a = 0;
-    
+    printf("이제 된다");
     printf("%d", a);
+    printf("%d", a+10);
     return 0;
 }
